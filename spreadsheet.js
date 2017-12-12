4d4cd57c6a888b4c6b85a1c24e5fcaedae372cdd
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2017-12-11 19:54:05 -0500

    little steps at a time

--- spreadsheet.js
+++ spreadsheet.js
@@ -46,7 +46,7 @@ rl.on('line', function(line){
 
     }
 
-    if(count > 32998 && count < 34000){
+    if(count > 65998 && count < 67000){
        //console.log(fName, lName, age, gender, zip);
         //console.log(fName, lName, gender, age, mailz, zip);
         console.log(fName+'\t'+lName+'\t'+gender+'\t'+age+'\t'+mailz+'\t'+zip);
