a49105cf45728bcbffb0369ca5edefbfff2347a7
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2017-12-12 23:16:22 -0500

    350

--- spreadsheet.js
+++ spreadsheet.js
@@ -46,7 +46,7 @@ rl.on('line', function(line){
 
     }
 
-    if(count > 301998 && count < 303000){
+    if(count > 348998 && count < 350000){
        //console.log(fName, lName, age, gender, zip);
         //console.log(fName, lName, gender, age, mailz, zip);
         console.log(fName+'\t'+lName+'\t'+gender+'\t'+age+'\t'+mailz);
